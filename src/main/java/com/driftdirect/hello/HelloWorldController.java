fc142efd38985a7781c3265514c37ae0f3b89cb6
pbochis <paul.bochis@catalysts.cc>
2015-11-06 09:15:15 +0200

    initial commit

---
+++ src/main/java/com/driftdirect/hello/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.driftdirect.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Paul on 11/6/2015.
+ */
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping(path = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value = "name", defaultValue = "World")String name){
+        return "Hello" + name;
+    }
+}
