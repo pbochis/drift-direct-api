2f5c23820d4494f2219412c0e1eb1b3bbcef698f
pbochis <paul.bochis@catalysts.cc>
2015-11-09 15:20:24 +0200

    Implement custom configurations  * add configuration for datasource & transactionManager  * adapt properties files  * add hibernate dependencies

---
+++ src/main/java/com/driftdirect/config/DatasourceConfig.java
@@ -0,0 +1,72 @@
+package com.driftdirect.config;
+
+import com.driftdirect.Application;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by Paul on 11/9/2015.
+ */
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackageClasses = Application.class)
+public class DatasourceConfig {
+    @Value("${datasource.url}")
+    private String url;
+    @Value("${datasource.driver}")
+    private String driver;
+    @Value("${datasource.username}")
+    private String username;
+    @Value("${datasource.password}")
+    private String password;
+    @Value("${hibernate.dialect}")
+    private String dialect;
+    @Value("${hibernate.ddl-auto}")
+    private String hbm2ddlAuto;
+    @Value("${hibernate.show_sql}")
+    private Boolean showSql;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource);
+        entityManager.setPackagesToScan("com.driftdirect");
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(Environment.DIALECT, dialect);
+        jpaProperties.put(Environment.HBM2DDL_AUTO, hbm2ddlAuto);
+        jpaProperties.put(Environment.SHOW_SQL, showSql);
+        entityManager.setJpaProperties(jpaProperties);
+
+        return entityManager;
+    }
+
+    public JpaTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManager){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManager.getObject());
+        return transactionManager;
+    }
+}
