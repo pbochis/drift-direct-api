dd2843adda99f0aa14ac872a93128b13a53bcf56
Paul Bochis <paul.bochis@gmail.com>
2016-01-19 17:59:30 +0200

    fixed team

--- src/main/java/com/driftdirect/dto/team/TeamShowDto.java
+++ src/main/java/com/driftdirect/dto/team/TeamShowDto.java
@@ -1,17 +1,11 @@
 package com.driftdirect.dto.team;
 
-import com.driftdirect.dto.sponsor.SponsorShowDto;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Paul on 11/26/2015.
  */
 public class TeamShowDto{
     private Long id;
     private String name;
-    private List<SponsorShowDto> sponsors;
 
     public String getName() {
         return name;
@@ -19,21 +13,6 @@ public class TeamShowDto{
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<SponsorShowDto> getSponsors() {
-        return sponsors;
-    }
-
-    public void setSponsors(List<SponsorShowDto> sponsors) {
-        this.sponsors = sponsors;
-    }
-
-    public void addSponsor(SponsorShowDto dto){
-        if (sponsors == null){
-            sponsors = new ArrayList<>();
-        }
-        sponsors.add(dto);
     }
 
     public Long getId() {
